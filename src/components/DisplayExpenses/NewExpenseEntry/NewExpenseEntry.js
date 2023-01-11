8bd5d45de6936bebd5d53d715627279eac1e43f4
Ergun Oz <ergun.oz82@gmail.com>
2023-01-10 22:52:28 -0500

    errorModal is added. The user will be notified When the user do not enter valid amount or/and missing info

--- src/components/DisplayExpenses/NewExpenseEntry/NewExpenseEntry.js
+++ src/components/DisplayExpenses/NewExpenseEntry/NewExpenseEntry.js
@@ -1,7 +1,10 @@
+import React from "react";
 import { useRef, useState } from "react";
+import ErrorModal from "../../UI/ErrorModal";
 import classes from "./NewExpenseEntry.module.css";
 
 const NewExpenseEntry = (props) => {
+  const [error, setError] = useState();
   const title = useRef();
   const amount = useRef();
   const date = useRef();
@@ -17,14 +20,43 @@ const NewExpenseEntry = (props) => {
       date: new Date(enteredDate),
       id: Math.random().toString(),
     };
+    if (
+      enteredTitle.trim().length === 0 ||
+      enteredAmount.trim().length === 0 ||
+      enteredDate.trim().length === 0
+    ) {
+      setError({
+        title: "Invalid or missing input",
+        message: "Please fill out all the parts",
+      });
+      return;
+    }
+    if (enteredAmount < 0.1) {
+      setError({
+        title: "Invalid amount ",
+        message: "enter a value grater than 0",
+      });
+      return;
+    }
     props.onUserInput(expenseData);
     title.current.value = "";
     amount.current.value = "";
     date.current.value = "";
   };
 
+  const errorHandler = () => {
+    setError(null);
+  };
+
   return (
     <div className={classes.card}>
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        />
+      )}
       <form onSubmit={submitHandler}>
         <div className={classes.form_controls}>
           <label>Title</label>
